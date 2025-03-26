1115ba17deaee2f15f11a26425c1d3721b87af20
Samuel Esezobor <samuel.esezobor076@gmail.com>
2025-03-26 07:30:40 +0100

    Commit creating other application dependency contracts

---
+++ NLPC_EPS_server.Application/Models/Email/EmailSMTP.cs
@@ -0,0 +1,10 @@
+﻿namespace NLPC_EPS_server.Application.Models.Email
+{
+    public class EmailSMTP
+    {
+        public string To { get; set; } = string.Empty;
+        public string From { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+    }
+}
