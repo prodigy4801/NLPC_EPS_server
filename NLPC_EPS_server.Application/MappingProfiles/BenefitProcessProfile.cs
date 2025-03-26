c6ba3781869ce039f69208be40a758b91da22545
Samuel Esezobor <samuel.esezobor076@gmail.com>
2025-03-26 07:29:50 +0100

    Commit Auto Mapping of domain model to data transfer model on the BenefitProcess and Company Model

---
+++ NLPC_EPS_server.Application/MappingProfiles/BenefitProcessProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using NLPC_EPS_server.Application.Features.BenefitProcess.Query.GetAllBenefitProcess;
+using NLPC_EPS_server.Application.Features.BenefitProcess.Query.GetBenefitProcessDetails;
+using NLPC_EPS_server.DAL;
+
+namespace NLPC_EPS_server.Application.MappingProfiles
+{
+    internal class BenefitProcessProfile : Profile
+    {
+        public BenefitProcessProfile()
+        {
+            CreateMap<BenefitProcess, BenefitProcessDTO>();
+            CreateMap<BenefitProcess, BenefitProcessDetailsDTO>();
+        }
+    }
+}
---
+++ NLPC_EPS_server.Application/MappingProfiles/CompanyProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using NLPC_EPS_server.Application.Features.Company.Query.GetAllCompany;
+using NLPC_EPS_server.Application.Features.Company.Query.GetCompanyDetails;
+using NLPC_EPS_server.DAL;
+
+namespace NLPC_EPS_server.Application.MappingProfiles
+{
+    public class CompanyProfile : Profile
+    {
+        public CompanyProfile()
+        {
+            CreateMap<Company, CompanyDTO>();
+            CreateMap<Company, CompanyDetailsDTO>();
+        }
+    }
+}
