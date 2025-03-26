56c81513fb80c34ed71ce189b5f8615c5a303bd8
Samuel Esezobor <samuel.esezobor076@gmail.com>
2025-03-26 07:19:50 +0100

    Commit implement exception handling for NotFound and BadRequest Exceptions

---
+++ NLPC_EPS_server.Application/Exceptions/BadRequestExceptions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+
+namespace NLPC_EPS_server.Application.Exceptions
+{
+    public class BadRequestExceptions : Exception
+    {
+        public List<string> ValidationErrors { get; set; }
+        public BadRequestExceptions(string message) : base(message) { }
+
+        public BadRequestExceptions(string message, ValidationResult validationResult) : base(message)
+        {
+            ValidationErrors = new();
+            foreach (var error in validationResult.Errors)
+            {
+                ValidationErrors.Add(error.ErrorMessage);
+            }
+        }
+    }
+}
---
+++ NLPC_EPS_server.Application/Exceptions/NotFoundExceptions.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation.Results;
+
+namespace NLPC_EPS_server.Application.Exceptions
+{
+    public class NotFoundExceptions : Exception
+    {
+        public NotFoundExceptions(string name, object key) : base($"{name} ({key}) was not Found")
+        {
+
+        }
+    }
+}
